b613198168c34f5dd194f80f6c8b14c4dc993dfb
Cristian Villa <CristianVilla@Outlook.com>
2024-07-22 20:00:30 -0500

    chore: configure some settings

--- LotoUrban/LotoUrban/settings.py
+++ LotoUrban/LotoUrban/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -112,9 +113,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 MEDIA_URL = "/media/"
@@ -124,3 +125,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Jazzmin settings
+JAZZMIN_SETTINGS = {
+    "side_title": "Test1",
+    "site_header": "Loto Urban",
+    "site_brand": "Loto Urban",
+    "site_logo": "TempLogo.png",
+}
+
