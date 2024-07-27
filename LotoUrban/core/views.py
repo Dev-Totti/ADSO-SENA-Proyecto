3161d904a4b3ffd38dd34a4fa2a44dee2337313f
Cristian Villa <CristianVilla@Outlook.com>
2024-07-26 22:04:56 -0500

    feat(core): update index view to include products

--- LotoUrban/core/views.py
+++ LotoUrban/core/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
 def index(request):
-    return render(request, "core/index.html", {})
+    products = Product.objects.all()
+
+    context = {
+        'products': products
+    }
+
+    return render(request, "core/index.html", context)
