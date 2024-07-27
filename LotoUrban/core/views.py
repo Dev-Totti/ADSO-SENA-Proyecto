69624f4ed8c37cf58664b0e95d853e901eccb676
Cristian Villa <CristianVilla@Outlook.com>
2024-07-27 16:15:57 -0500

    refactor(core): update temp home page to /temp

--- LotoUrban/core/views.py
+++ LotoUrban/core/views.py
@@ -3,11 +3,15 @@ from django.shortcuts import render
 from .models import Product
 
 
-def index(request):
+def home(request):
+    return HttpResponse("Hello, World!")
+
+
+def temp(request):
     products = Product.objects.all()
 
     context = {
         'products': products
     }
 
-    return render(request, "core/index.html", context)
+    return render(request, "core/temp.html", context)
